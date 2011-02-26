1cb432c46823494c0b115b6751432088e0ca884b
Brian Morton <bmorton@sdreader.com>
2011-02-25 17:13:56 -0800

    Clean up order of code, add comments, add debug mode.

--- euler17/main.m
+++ euler17/main.m
@@ -11,27 +11,36 @@
 
 int main (int argc, const char * argv[])
 {
+    // Confirm we have the proper number of arguments
+    if (argc < 2) {
+        printf("usage: euler17 [number] [-debug]\n\n");
+        return 0;
+    }
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
+    // Setup the object that makes the conversion magic happen
     EnglishNumber *number = [[EnglishNumber alloc] init];
     
+    // Hold a sum of all digits in our loop
     int sum = 0;
     
-    if (argc != 2) {
-        printf("usage: euler17 [number]\n\n");
-        return 0;
-    }
-    
+    // Loop through int from 1 to the number passed in from the cli
     for (int i = 1; i <= [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] intValue]; i++) {
         number.number = i;
-        //NSLog(@"%@", [number string]);
+        
+        // If we throw a random flag at the end of our command, show this full debug output.
+        if (argc == 3) {
+            NSLog(@"%@", [number string]);
+        }
+        
+        // Sum up our letters
         sum += [number numberOfLetters];
     }
-                                     
+
     printf("Sum of all letters: %i\n\n", sum);
     
-    
+    // Get rid of the magic object
     [number release];
 
     [pool drain];
